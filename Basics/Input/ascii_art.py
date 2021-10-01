2b76ef17900474988f71f36bd82b81798747591d
Kieran Huntley <5huntk04@solent.ac.uk>
2021-10-01 12:23:19 +0100

    Added code to display an ASCII robot with dynamic eyes.

---
+++ Basics/Input/ascii_art.py
@@ -0,0 +1,9 @@
+print("enter a value for the eyes")
+eyes = input()
+
+print("Beeps expression is now as follows:")
+print(" ##########")
+print(f" #  {eyes}  {eyes}  #")
+print(" #  ----  #")
+print(" ##########")
+
