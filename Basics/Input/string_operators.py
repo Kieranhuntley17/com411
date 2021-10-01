1de476fad92e3de3d58f9672b356e4c45a22cddb
Kieran Huntley <5huntk04@solent.ac.uk>
2021-10-01 12:46:54 +0100

    creating health, shield and life (INCOMPLETE)

---
+++ Basics/Input/string_operators.py
@@ -0,0 +1,5 @@
+# user prompt for entering life count
+lives = int(input("Enter the number of lives: "))
+# user prompt for entering energy count
+int(input("Enter energy level"))
+# shield level next
