b4c8b4570da29fb7ed39df905a64342ee993394d
BuildTools <unconfigured@null.spigotmc.org>
2021-10-05 15:50:57 +0100

    multiple conditions added

--- Basics/Decisions/multiple_conditions.py
+++ Basics/Decisions/multiple_conditions.py
@@ -5,6 +5,7 @@ is_sunny = input().lower()
 print("is there a breeze")
 is_breeze = input().lower()
 
+# or can be used instead of and. this means only one of the conditions need to be true
 if is_sunny == "yes" and is_breeze == "yes":
     print("clothes are dry")
 else:
