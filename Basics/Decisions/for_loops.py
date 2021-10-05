77ee720fa0af0bc6eb7d7e3b169c62f8dd9458f5
BuildTools <unconfigured@null.spigotmc.org>
2021-10-05 16:20:37 +0100

    for loops and while loops introduction

---
+++ Basics/Decisions/for_loops.py
@@ -0,0 +1,5 @@
+# for loop starts the var count at 0 and goes through itself meaning you don't need to tell the program to update
+# how many numbers have been entered
+for count in range(5):
+    print("please enter a number")
+    user_number = float(input())
---
+++ Basics/Decisions/while_loops.py
@@ -0,0 +1,11 @@
+# while is like if but it carries on while the condition is true
+count = 0
+
+while count < 5:
+    print("please enter a number")
+    user_number = float(input())
+    count += 1
+
+print ("Program ended")
+
+# for
